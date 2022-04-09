13c5ddbb5a48dd10caa6687a2af3c8ca75eaabea
ayden610 <70598746+ayden610@users.noreply.github.com>
2022-04-09 16:35:21 -0400

    Main + Examples

---
+++ src/Examples.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Examples {
+
+    public Examples(){}
+    
+
+    // This shows what a test case looks like
+    @Test 
+    public void simpleCheck() {
+	assertEquals(4, 4);
+    }
+
+
+}
